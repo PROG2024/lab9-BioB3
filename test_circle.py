224044d246e87f6f379a48e1cd4beac5ae34ae00
Phasin Sae-ngow <bird.biob3@gmail.com>
2024-03-12 15:44:06 +0700

    add test_add_area_positive

--- test_circle.py
+++ test_circle.py
@@ -8,5 +8,22 @@ Write unit tests as described in README.md.
 
 """
 from circle import Circle
+import unittest
+import math
 
 # TODO write 3 tests as described above
+
+class CircleTest(unittest.TestCase):
+    """Tests for the Circle class"""
+    def setUp(self):
+        """create circles before each test"""
+        self.circle_five = Circle(5)
+        self.circle_ten = Circle(10)
+
+    def test_add_area_positive(self):
+        """add_area return a new circle whose area equal to the combined of two circles"""
+        new_circle = self.circle_five.add_area(self.circle_ten)
+        self.assertEqual(self.circle_five.get_area()+self.circle_ten.get_area(),
+                         new_circle.get_area())
+        self.assertEqual(math.hypot(self.circle_five.get_radius(),self.circle_ten.get_radius()),
+                         new_circle.get_radius())
