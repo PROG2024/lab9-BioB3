24b14438ca388debdd400c597a205de4f89c087e
Phasin Sae-ngow <bird.biob3@gmail.com>
2024-03-13 11:55:10 +0700

    add tests for Counter

--- counter/test_counter.py
+++ counter/test_counter.py
@@ -7,3 +7,31 @@
 
    You can use pytest or unittest.
 """
+
+from counter import Counter
+import unittest
+
+class CounterTest(unittest.TestCase):
+    """Test for the Counter class"""
+
+    def setUp(self):
+        """Create a new Counter before each test"""
+        self.counter_1 = Counter()
+        self.counter_2 = Counter()
+
+    def test_create_another_instance(self):
+       """__new__ return the same Counter"""
+       self.assertEqual(id(self.counter_1), id(self.counter_2))
+       self.assertEqual(self.counter_1.count, self.counter_2.count)
+
+    def test_increment(self):
+       """Counter's count increase when increment is called
+       regardless of which variable is used
+       """
+       self.counter_1.increment()
+       self.assertEqual(1, self.counter_2.count)
+       self.assertEqual(self.counter_1.count, self.counter_2.count)
+       self.counter_2.increment()
+       self.assertEqual(2, self.counter_1.count)
+       self.assertEqual(self.counter_1.count, self.counter_2.count)
+   
